18ad27f5be5eb5144d209db4bdc57f90267189e2
aruzo <dawid12391111@gmail.com>
2024-02-25 17:51:41 +0100

    add tests done count

--- pages/api/complete-test.ts
+++ pages/api/complete-test.ts
@@ -1,5 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { TestsCrawler } from "@/lib/tests-crawler";
+import fs from "fs";
+import path from "path";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (
@@ -21,6 +23,14 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       req.body.url,
       req.body.correctAnswers,
     );
+
+    let testsDoneCount = 0;
+
+    try {
+      testsDoneCount = parseInt(fs.readFileSync("./count").toString(), 10);
+    } catch {}
+
+    fs.writeFileSync("./count", (testsDoneCount + 1).toString());
 
     res.status(200).json(resultUrl);
   } catch (e) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,25 @@
 import { CompleteTestForm, TestsList } from "@/components";
+import { GetServerSideProps, NextPage } from "next";
+import fs from "fs";
 
-const HomePage = () => {
+const HomePage: NextPage<{ testsDoneCount: string }> = ({ testsDoneCount }) => {
   return (
     <div className="container py-16">
       <CompleteTestForm />
       <TestsList />
+      <span className="mt-16 block text-xl font-bold">{testsDoneCount}</span>
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  let testsDoneCount = "0";
+
+  try {
+    testsDoneCount = fs.readFileSync("./count").toString();
+  } catch {}
+
+  return { props: { testsDoneCount } };
 };
 
 export default HomePage;
